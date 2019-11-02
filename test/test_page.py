630887402283299d17f9d28f1adc77a64a040330
Michael Bukachi <michaelbukachi@gmail.com>
2019-11-02 16:43:29 +0300

    build: Improve wait time for logic for the https server

--- test/test_page.py
+++ test/test_page.py
@@ -3,7 +3,5 @@ from contextlib import closing
 
 
 def test_text_is_show(driver):
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-        if sock.connect_ex(('localhost', 8000)) == 0:
-            driver.get('http://localhost:8000')
-            assert 'This is just a demo.' in driver.page_source
+    driver.get('http://localhost:8000')
+    assert 'This is just a demo.' in driver.page_source
