5d55592fcbb4418cc1be9699d58bfd97956abd93
Michael Bukachi <michaelbukachi@gmail.com>
2021-05-16 02:35:31 +0300

    refactor: Update chromedriver setup

--- test/conftest.py
+++ test/conftest.py
@@ -11,6 +11,11 @@ def path():
 
 @pytest.fixture(scope='session')
 def driver(path):
-    driver_ = webdriver.Chrome(path)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver_ = webdriver.Chrome(path, chrome_options=chrome_options)
     yield driver_
     driver_.quit()
